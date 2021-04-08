7f8ea1307f8450e2f71b6a6d0bcdceec91134717
Tristan Eason <tristaneason@protonmail.com>
2021-04-08 18:28:14 +0000

    Last commit on the shared server

--- wp-content/themes/porto-child/functions.php
+++ wp-content/themes/porto-child/functions.php
@@ -5,3 +5,11 @@ function add_theme_scripts() {
     // wp_enqueue_script('script', get_template_directory_uri() . '/js/script.js', '', 1.1, true);
 }
 add_action('wp_enqueue_scripts', 'add_theme_scripts');
+
+function add_free_shipping_label($label, $method) {
+    if ($method->cost == 0) {
+        $label = 'Free Shipping';
+    }
+    return $label;
+}
+add_filter('woocommerce_cart_shipping_method_full_label', 'add_free_shipping_label', 10, 2);
